0c2e2a5582555f1eb39f20f2114301f67f7244f3
ianlombu <ianlombu09@gmail.com>
2020-04-24 00:29:00 +0700

    User Registration and Login Authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('customer/<str:id>', views.customer, name='customer'),
     path('create_order/<str:id>', views.createOrder, name='create_order'),
     path('update_order/<str:id>', views.updateOrder, name='update_order'),
-    path('delete_order/<str:id>', views.deleteOrder, name='delete_order')
+    path('delete_order/<str:id>', views.deleteOrder, name='delete_order'),
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+    path('register/', views.registerPage, name='register')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,72 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.forms import inlineformset_factory
+
 from .models import *
-from .forms import OrderForm
+
+from django.forms import inlineformset_factory
+from .forms import OrderForm, CreateUserForm
 from .filters import OrderFilter
+from django.contrib.auth.forms import UserCreationForm
+
+from django.contrib.auth import authenticate, login, logout
+
+# hampir sama seperti middleware ========>>> lanjutannya adalah @login_required(login_url='login') ======>> login terlebih dahulu
+from django.contrib.auth.decorators import login_required
+# ============================================================
+
+# ==========FLASH MESSAGES ==================
+from django.contrib import messages
 
 
+def registerPage(request):
+    # form = UserCreationForm()
+    # ===============================================
+    if request.user.is_authenticated:
+        return redirect('home')
+    # ===============================================
+    else:
+        form = CreateUserForm()
+        if request.method == "POST":
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                # FLASSH MESSAGES
+                messages.success(request, 'Account was created for ' + user)
+                return redirect('login')
+
+    context = {'form': form}
+    return render(request, 'accounts/register.html', context)
+
+
+def loginPage(request):
+    # jika sudah login maka tidak bisa masuk ke logi lagi
+    # ===============================================
+    if request.user.is_authenticated:
+        return redirect('home')
+    # ===============================================
+    else:
+        if request.method == "POST":
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Username or Password is incorrect')
+
+        context = {}
+        return render(request, 'accounts/login.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+
+@login_required(login_url='login')
 def home(request):
     customers = Customer.objects.all()
     orders = Order.objects.all()
@@ -17,12 +78,14 @@ def home(request):
     return render(request, 'accounts/dashboard.html', context)
 
 
+@login_required(login_url='login')
 def product(request):
     products = Product.objects.all()
     context = {'products': products}
     return render(request, 'accounts/products.html', context)
 
 
+@login_required(login_url='login')
 def customer(request, id):
     customers = Customer.objects.get(id=id)
     orders = customers.order_set.all()
@@ -39,6 +102,7 @@ def customer(request, id):
     return render(request, 'accounts/customer.html', context)
 
 
+@login_required(login_url='login')
 def createOrder(request, id):
     # extra = 5 =======>>>>>> menampilkan form sebanyak 5
     OrderFormSet = inlineformset_factory(
@@ -60,6 +124,7 @@ def createOrder(request, id):
     return render(request, 'accounts/create_order.html', context)
 
 
+@login_required(login_url='login')
 def updateOrder(request, id):
     order = Order.objects.get(id=id)
     form = OrderForm(instance=order)
@@ -74,6 +139,7 @@ def updateOrder(request, id):
     return render(request, 'accounts/update_order.html', context)
 
 
+@login_required(login_url='login')
 def deleteOrder(request, id):
     order = Order.objects.get(id=id)
     if request.method == "POST":
